cbca0bc9aa358d502566ca1c969660dc068d945d
Rawmver <139946633+Rawmver@users.noreply.github.com>
2024-03-23 11:47:47 +0500

    Update config.js

--- config.js
+++ config.js
@@ -22,11 +22,11 @@ global.APIs = {
 
 // Other
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = '𝑆𝛪𝐷 𝐵𝛨𝛥𝛪';
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = '𝐿𝑈𝐶𝛪𝐹𝛯𝑅';
+global.owner = ['923082990885'];
+global.premium = ['923082990885'];
 global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
-global.author = '𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫';
+global.author = '𝙇𝙐𝘾𝞘𝙁𝞢𝞒';
 global.sessionName = 'session';
 global.prefa = ['!', '.'];
 global.sp = '✪';
